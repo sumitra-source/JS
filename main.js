91c6d1318782baa357c9a08bbbc0d120a734ba50
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-02-23 20:25:47 +0545

    jscc

---
+++ main.js
@@ -0,0 +1,205 @@
+//log the statement using console.log
+//console.log('Hello from main.js')
+ //node mainconst age = 25
+ //var age=20
+ //console.log(age)
+ //console.log(age)
+ //let sum = 0
+// sum=5
+ //console.log(sum)
+ /*
+ const name='Sumitra'
+ const language='JavaScript'
+ const channel='Codeevolution'
+
+ const total=0
+ const PI=3.14
+
+ const isPrimaryNumber=true
+ const isNewUser=false
+ 
+ let result
+ console.log(result)
+
+ const res=undefined
+ const res=null
+ const data=null
+ */
+/*const person={
+    'firstName':'Bruce',
+    'lastName':'Wayne',
+    'age':30
+}
+console.log(person.firstName)*/
+/*
+const oddNumber=[1,3,5,7,9]
+console.log(oddNumber[0])
+let a=10
+a='Sumitra'
+a=true
+console.log(a)*/
+/*
+let x=10
+let y=5
+console.log(x+y)
+console.log(++x)
+console.log(--y)
+console.log(x==y)
+console.log(x!=y)
+console.log(x===y)
+console.log(x!==y)
+console.log(x>y)
+console.log(x>=y)
+console.log(y<=x)
+
+*/
+/*
+let x=10
+let  y=5
+const isValidNumber=x>8 && 8>y
+console.log(isValidNumber)
+const isValid=true
+console.log(!isValid)
+console.log('Bruce ' + ' Wayne')
+const isEven=10%2==0?true:false
+console.log(isEven)*/
+/*
+const isEven =10%2==0?'Number is even':'Number is odd'
+console.log(isEven)
+console.log(2+'3')
+console.log(true+'3')
+console.log('4'-'2')
+console.log('Bruce'+'Wayne')
+console.log('5'-true)
+console.log('5'-false)
+console.log('5'-null)
+console.log(5+undefined)
+console.log(Number('5'))
+console.log(Number(false))
+console.log(parseInt('5'))
+console.log(parseFloat('3.14'))
+console.log(String(500))
+console.log(String(null))
+console.log(String(undefined))
+console.log((500).toString)
+console.log(Boolean(10))
+*/
+/*
+const var1='test'
+const var2='test'
+console.log(var1==var2)
+console.log(var1===var2)*/
+/*
+const var1=false
+const var2=false
+console.log(var1==var2)
+console.log(var1===var2)*/
+/*
+const var1=null
+const var2=undefined
+console.log(var1==var2)
+console.log(var1===var2)*/
+
+/*
+
+const num=5
+if(num>0){
+    console.log('Number is positive');
+
+}
+else if(num<0)
+{
+    console.log('Number is negative')
+
+}
+else{
+    console.log('Number is zero')
+}
+*/
+/*
+const color='red'
+switch(color)
+{
+    case 'red':
+        console.log('color is red')
+        break
+        case 'blue':
+        console.log('Color is blue')
+        break
+        case'green':
+        console.log('color is green')
+        break
+        default:
+            console.log('Not a valid color')
+}*/
+/*
+
+for(let i=1;i<=5;i++){
+    console.log('Iteration number'+i)
+}
+let i=6
+while(i<=5){
+    console.log('Iteration Number'+i)
+    i++
+}
+do{
+    console.log('Iteration number'+i)
+    i++
+
+}while(i<=5)
+
+const numArray=[1,2,3,4,5]
+
+
+for(const num of numArray){
+    console.log('Iteration number'+num)
+}*/
+
+/*
+
+function greet(username)
+{
+    console.log('Good Morning '+username)
+}
+greet('Sumitra')
+
+function add(a,b)   
+{
+    return a+b
+}
+const sum=add(5,10)
+console.log(sum)*/
+/*
+
+const arrowSum=(a,b)=>{
+    return a+b
+}
+
+const sum=arrowSum(25,25)
+console.log(sum)
+*/
+
+/*
+const arrowSum=(a,b)=>a+b
+
+const addFive=(num)=>num+5
+const sum=arrowSum(25,25)
+console.log(sum)
+*/
+const myNum=100
+const myName='Superman'
+
+if(true){
+    const myName='Sumitra'
+    console.log(myName)
+    console.log(myNum)
+}
+
+function testfn(){
+    const myName='Batman'
+    console.log(myName)
+    console.log(myNum)
+}
+testfn()
+
+

862bdd4f9421728b7c2484879a81cc7a69f06911
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-03-03 10:37:35 +0545

    d4

--- React/hello-world/src/components/Greet.js
+++ React/hello-world/src/components/Greet.js
@@ -3,15 +3,29 @@ import React from 'react'
 function Greet(){
     return <h1>Hello Sumitra</h1>
 }*/
-const Greet= props =>
+{/*
+const Greet= ({name,heroName}) =>
 {
-    console.log(props)
+    
     return(   
         <div> 
             <h1>
-                Hello {props.name} a.k.a. {props.heroName}
+                Hello {name} a.k.a. {heroName}
             </h1>
-        {props.children}
+   
+    </div>
+    )
+}*/}
+const Greet= props=>
+{
+    const{name,heroName}=props
+    
+    return(   
+        <div> 
+            <h1>
+                Hello {name} a.k.a. {heroName}
+            </h1>
+   
     </div>
     )
 }
